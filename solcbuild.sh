fb66315b62596479c5d1a008a81ce71e391caa66
bradleecoder <limingming@visiontech-ltd.com>
2021-02-25 14:58:27 +0800

    x

--- solcbuild.sh
+++ solcbuild.sh
@@ -5,7 +5,7 @@ if [ ! -d "pancake-swap-lib" ]; then
   git clone https://github.com/pancakeswap/pancake-swap-lib.git
 fi
 
-solc @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/ --bin --abi --optimize -o ./abibin MasterChef.sol
+solc @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/ --bin --abi --optimize --overwrite -o ./abibin MasterChef.sol
 
 
 
