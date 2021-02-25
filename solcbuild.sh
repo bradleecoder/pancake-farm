8742f2a94b2aa9b77afc7332c1ff4d6f2d179369
bradleecoder <limingming@visiontech-ltd.com>
2021-02-25 15:13:44 +0800

    x

--- solcbuild.sh
+++ solcbuild.sh
@@ -7,7 +7,11 @@ fi
 
 solc @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/ --bin --abi --optimize --overwrite -o ./abibin MasterChef.sol
 
+abigen --sol=MasterChef.sol --solc='solc @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/' --pkg=pancakeswap --out=MasterChef.go
 
+abigen @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/ --sol=MasterChef.sol --pkg=pancakeswap --out=MasterChef.go
+
+solc @pancakeswap/pancake-swap-lib/=./pancake-swap-lib/ --bin --abi --optimize --overwrite -o ./abibin MasterChef.sol | abigen --pkg=pancakeswap --out=MasterChef.go
 
 
 
